c9120ca63a885ca45950c62dbf16bba75c7be795
svob <svoboda.filip@gmail.com>
2017-04-26 18:16:02 +0200

    Add models and admin

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import IssueCategory, Issue, IssueState
+
 
 # Register your models here.
+class IssueAdmin(admin.ModelAdmin):
+    fields = ['description', 'category', 'state', 'solver', 'created', 'finished']
+    list_display = ('description', 'category', 'submitter', 'solver', 'state', 'created', 'finished')
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.submitter = request.user
+        obj.save()
+
+admin.site.register(IssueCategory)
+admin.site.register(Issue, IssueAdmin)
--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+class IssueState(models.Model):
+    state = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.state
+
+class IssueCategory(models.Model):
+    category = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category
+
+    class Meta:
+        verbose_name_plural = 'Issue categories'
+
+class Issue(models.Model):
+    submitter = models.ForeignKey(User, related_name='%(class)s_created')
+    solver = models.ForeignKey(User, related_name='%(class)s_solved')
+    description = models.CharField(max_length=500)
+    state = models.ForeignKey(IssueState)
+    category = models.ForeignKey(IssueCategory)
+    created = models.DateTimeField(default=timezone.now)
+    finished = models.DateTimeField(null=True, blank=True)
